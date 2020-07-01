98939e310ee3073191a82a05f165bf4c2ab54779
goodbaduglyboy <35921411+goodbaduglyboy@users.noreply.github.com>
2020-07-01 07:58:25 +0530

    Update 8_Queens.py

--- 8_Queens.py
+++ 8_Queens.py
@@ -1,11 +1,11 @@
 #Progam to find a solution for 8 Queens problem
 import random
 import sys
-
-global n,maxFitness,pop_size
+import numpy as np
 
 #Function to generate initial population
 def populate():
+  global n,maxFitness,pop_size
   pop_list=[]
   n=8
   maxFitness = (n*(n-1))/2 
@@ -13,7 +13,7 @@ def populate():
   for i in range(pop_size):
     population = random.sample(range(0,n), n)
     pop_list.append(population)
-  # print(pop_list)
+  
   return pop_list
 
 #Function to select randomly from population
@@ -22,12 +22,12 @@ def rand_select(pop_list):
   fitscore = calc_fitness(selected_element)
   return(selected_element,fitscore)
 
-#Function to reproduce two randomly selected chromosomes
+#Function to reproduce two chromosomes
 def reproduce(x,y):
   n = len(x)
   c = random.randint(0, n - 1)
   child= x[0:c] + y[c:n]
-  # print(child)
+  
   return child
 
 #Function to mutate a child
@@ -38,45 +38,42 @@ def mutate(child):
   child[c] = m
   mutated_child= child
   return mutated_child
-  # print('mutated-',child)
+  
 
 #Function to calculate Fitness
 def calc_fitness(chrome):
-  horizontal_collisions = sum([chrome.count(queen)-1 for queen in chrome])/2
-  # print(chrome,[chrome.count(queen)-1 for queen in chrome],horizontal_collisions)
-  diagonal_collisions = 0
+  l = chrome
+  clashes = 0
+  row_clashes = abs(len(l)-len(np.unique(l)))
+  
 
-  n = len(chrome)
-  left_diagonal = [0] * 2*n
-  right_diagonal = [0] * 2*n
-  for i in range(n):
-      left_diagonal[i + chrome[i] - 1] += 1
-      right_diagonal[len(chrome) - i + chrome[i] - 2] += 1
-  diagonal_collisions = 0
-  for i in range(2*n-1):
-      counter = 0
-      if left_diagonal[i] > 1:
-          counter += left_diagonal[i]-1
-      if right_diagonal[i] > 1:
-          counter += right_diagonal[i]-1
-      diagonal_collisions += counter / (n-abs(i-n+1))
-  fit= maxFitness - (horizontal_collisions + diagonal_collisions)
-  return(fit)
+  for i in range(len(l)):
+    for j in range(len(l)):
+      if(i!=j):
+        dx = abs(i-j)
+        dy = abs(l[i]-l[j])
+        if(dx==dy):
+          clashes=clashes+1
+
+  clashes= clashes+row_clashes
+  fit= maxFitness - clashes
+  return fit
+  
 
 #Genetic Algorithm function
 def genetic_queen(pop_list):
   new_population = []
 
   for i in range(0,pop_size):
-    x,fitx = rand_select(pop_list)
+    x,fitx = rand_select(pop_list) #Selection at work
     y,fity = rand_select(pop_list)
 
     child = reproduce(x,y) #Reproduction is done here
-    mutated_child = mutate(child) #Mutation is done here
-    new_population.append(mutated_child)
-    mut_fitness = calc_fitness(mutated_child)
+    child = mutate(child) #Mutation is done here
+    new_population.append(child)
+    mut_fitness = calc_fitness(child)
     if  mut_fitness == maxFitness: 
-      # print(mutated_child,mut_fitness,maxFitness)
+  
       nofitfound= False
       break
     else:
@@ -84,12 +81,13 @@ def genetic_queen(pop_list):
     
   return(new_population,nofitfound)
 
-#Main Function
+# Main Function
 if __name__ == "__main__":
+  
   pop_list=populate()
   nofitfound =  False
   new_population,nofitfound=genetic_queen(pop_list)
-  # print(nofitfound)
+  
   while nofitfound == True:
     new_population,nofitfound=genetic_queen(pop_list)
   for chrom in new_population:
